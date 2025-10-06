d955337813e8ebba052ab30bee554ed57df1a8bd
Koleno33 <shanyacat@yandex.ru>
2025-10-06 21:30:29 +0300

    camera move system

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <raymath.h>
 #include <vector>
 #include <memory>
 #include <graphics/cube.h>
@@ -12,11 +13,25 @@ int main(void)
   InitWindow(screenWidth, screenHeight, "AI Ship Room Designer");
 
   Camera camera = { 0 };
-  camera.position = (Vector3){ 10.0f, 5.0f, 0.0f };    // Позиция камеры
-  camera.target = (Vector3){ -1.5f, 0.0f, -1.5f };      // Куда смотрит камера
+  camera.position = (Vector3){ 10.0f, 5.0f, 0.0f };   // Позиция камеры
+  camera.target = (Vector3){ 0.0f, 5.0f, 10.0f };      // Куда смотрит камера
   camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Вверх (обычно вектор (0,1,0))
   camera.fovy = 45.0f;                                // Поле зрения
   camera.projection = CAMERA_PERSPECTIVE;             // Тип проекции
+
+  // Начальные углы
+  Vector3 direction = Vector3Subtract(camera.target, camera.position);
+  direction = Vector3Normalize(direction);
+
+  // Управление камерой
+  Vector2 prev_mouse_pos = { 0 };
+  float camera_yaw = atan2f(direction.z, direction.x);
+  float camera_pitch = asinf(direction.y);
+  float mousesens = 0.003f;
+  float movspeed = 0.09f;
+
+  HideCursor();
+  DisableCursor();
  
   SetTargetFPS(60);
 
@@ -38,8 +53,56 @@ int main(void)
   CubeValidator validator { };
 
   while (!WindowShouldClose()) {
-    // Обновление
-    UpdateCamera(&camera, CAMERA_ORBITAL); // Вращение камеры вокруг объекта
+    Vector2 cur_mouse_pos = GetMousePosition();
+    Vector2 mouse_delta = {
+      (cur_mouse_pos.x - prev_mouse_pos.x) * mousesens,
+      (cur_mouse_pos.y - prev_mouse_pos.y) * mousesens
+    };
+
+    camera_yaw += mouse_delta.x;
+    camera_pitch -= mouse_delta.y;
+
+    // Чтобы камера на перевернулась
+    if (camera_pitch > 89.0f * DEG2RAD) camera_pitch = 89.0f * DEG2RAD;
+    if (camera_pitch < -89.0f * DEG2RAD) camera_pitch = -89.0f * DEG2RAD;
+
+    Vector3 forward = {
+        cosf(camera_yaw) * cosf(camera_pitch),
+        0,  // Игнорируем вертикальную составляющую для горизонтального движения
+        sinf(camera_yaw) * cosf(camera_pitch)
+    };
+    forward = Vector3Normalize(forward);
+    
+    Vector3 right = {
+        -forward.z,
+        0.0f,
+        forward.x
+    };
+    right = Vector3Normalize(right);
+
+    if (IsKeyDown(KEY_W)) camera.position = Vector3Add(camera.position, Vector3Scale(forward, movspeed));
+    if (IsKeyDown(KEY_S)) camera.position = Vector3Subtract(camera.position, Vector3Scale(forward, movspeed));
+    if (IsKeyDown(KEY_D)) camera.position = Vector3Add(camera.position, Vector3Scale(right, movspeed));
+    if (IsKeyDown(KEY_A)) camera.position = Vector3Subtract(camera.position, Vector3Scale(right, movspeed));
+    if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) 
+        camera.position.y -= movspeed;
+    if (IsKeyDown(KEY_SPACE)) 
+        camera.position.y += movspeed;
+
+    // Рассчитываем новый target на основе углов
+    Vector3 new_direction = {
+      cosf(camera_yaw) * cosf(camera_pitch),
+      sinf(camera_pitch),
+      sinf(camera_yaw) * cosf(camera_pitch)
+    };
+    new_direction = Vector3Normalize(new_direction);
+    camera.target = Vector3Add(camera.position, new_direction);
+
+    // Обновляем позицию мыши для следующего кадра
+    prev_mouse_pos = cur_mouse_pos;
+
+    // Обработка выхода из программы
+    if (IsKeyPressed(KEY_Q)) break;
 
     Color valcolor = validator.validate_cubes(cubes);
 
