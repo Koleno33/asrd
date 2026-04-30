18a7cf31e3cf6c1587252d5b328d2bd015886519
Koleno33 <shanyacat@yandex.ru>
2026-04-30 15:12:24 +0300

    style: add some indentation at Sphere header

--- include/graphics/sphere.h
+++ include/graphics/sphere.h
@@ -8,19 +8,20 @@ private:
   inline static constexpr int DEFAULT_RINGS = 16;
   inline static constexpr int DEFAULT_SLICES = 16;
 
-  float   radius {};
+  float radius {};
 
 public:
   Sphere(const Vector3& pos, float radius, Color color = WHITE);
   
   const char* get_type() const override;
-  void        draw() const override;
-  
-  float   get_radius() const { return radius; };
-  void    set_radius(float newradius);
 
-  float   calculate_distance(const Object& other) const override;
-  bool    check_collision(const Object& other) const override;
+  void   draw() const override;
+  
+  float  get_radius() const { return radius; };
+  void   set_radius(float newradius);
+
+  float  calculate_distance(const Object& other) const override;
+  bool   check_collision(const Object& other) const override;
 
   float  calculate_distance_to_wall(const Wall& wall) const override;
 
